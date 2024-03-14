321eb1145c3a47af21298cc9afce9ba439787886
Ujjwal kumar <149386327+ujjwali2s@users.noreply.github.com>
2024-03-15 03:45:24 +0530

    Delete client directory

--- client/src/pages/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import CallToAction from '../components/CallToAction';
-
-export default function Projects() {
-  return (
-    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
-      Loading our projects
-    </div>
-  )
-}
